8eaf5bc33ef783aec0b4351744393949aefe3b43
Vu Cao <cdhvu@yahoo.com>
2019-02-28 09:49:11 -0800

    Pulling and displaing data on table with basic functionalities.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,78 @@
 import React, { Component } from "react";
+import axios from "axios";
+import MaterialTable from "material-table";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    users: [],
+    newPhone: "",
+    newEmail: "",
+    currentPhone: "",
+    currentEmail: ""
+  };
+
+  async componentDidMount() {
+    const { data } = await axios.get("http://localhost:5000");
+    const rowsData = data.map(row => {
+      return {
+        id: row.id,
+        name: row.name,
+        phone: row.phone,
+        email: row.email,
+        tableData: row.tableData
+      };
+    });
+
+    this.setState({ users: rowsData });
+  }
+
+  changeHandler = async e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
-    return <div className="App">Hello</div>;
+    return (
+      <div className="App">
+        <MaterialTable
+          columns={[
+            {
+              title: "Name",
+              field: "name"
+            },
+            {
+              title: "Phone Number",
+              field: "phone",
+              sorting: false,
+              render: rowData => rowData.phone
+            },
+            { title: "Email", field: "email", render: rowData => rowData.email }
+          ]}
+          data={this.state.users}
+          title="Users Table"
+          actions={[
+            {
+              icon: "edit_icon",
+              tooltip: "Edit User Info",
+              onClick: this.editRow
+            },
+            {
+              icon: "save_icon",
+              tooltip: "Save User Info",
+              onClick: this.updateRow
+            }
+          ]}
+          options={{
+            paging: true,
+            pageSize: 20,
+            pageSizeOptions: [5, 10, 20, 50, 100],
+            sorting: true,
+            actionsColumnIndex: -1,
+            exportButton: true
+          }}
+        />
+      </div>
+    );
   }
 }
 
